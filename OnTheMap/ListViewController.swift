d91ebbc30036e99b0da9204e3d187e1913eca993
Victor Guerra <vguerra@gmail.com>
2015-06-16 09:59:40 +0200

    Adding Controllers for Map and List view of pins

---
+++ OnTheMap/ListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ListViewController.swift
+//  OnTheMap
+//
+//  Created by Victor Guerra on 16/06/15.
+//  Copyright (c) 2015 Victor Guerra. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+}
---
+++ OnTheMap/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by Victor Guerra on 16/06/15.
+//  Copyright (c) 2015 Victor Guerra. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController : UIViewController, MKMapViewDelegate {
+
+
+
+}
