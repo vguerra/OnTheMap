865140684ea4e211b76af1661168b2fe1bd76709
Victor Guerra <vguerra@gmail.com>
2015-06-19 11:54:19 +0200

    Moving constants out of the AppDelegate object into the Constants struct in the Api client class.

--- OnTheMap/AppDelegate.swift
+++ OnTheMap/AppDelegate.swift
@@ -13,10 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    let parseAppId : String = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
-    let parseRESTAPIKey : String = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
-
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         return true
     }
