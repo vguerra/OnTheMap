b8e99e2eecb3633b087331dbbe8fba032860ce75
Victor Guerra <vguerra@gmail.com>
2015-07-27 00:52:06 +0200

    Using all functionality defined on SLViewController and conforming to protocols needed by the Table View to show student locations and open their provided links.

--- OnTheMap/ListViewController.swift
+++ OnTheMap/ListViewController.swift
@@ -8,51 +8,60 @@
 
 import UIKit
 
-class ListViewController : UIViewController, UITableViewDataSource, UITableViewDelegate, CommonNavigationBar {
+class ListViewController : SLViewController, UITableViewDataSource, UITableViewDelegate, CommonNavigationBar {
     
+
+    @IBOutlet weak var locationsTable: UITableView!
+    
+    // MARK: ViewController life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         addNavigationBar(self)
         self.navigationItem.title = "On The Map"
     }
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if APIClient.sharedInstance.studentLocations == nil {
+            self.refresh()
+        }
+    }
     
     // MARK: Conforming to UITableViewDataSource
-    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        if let count = APIClient.sharedInstance.studentLocations?.count {
+            return count
+        }
+        return 0
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let studentCell = tableView.dequeueReusableCellWithIdentifier("StudentLocationCell", forIndexPath: indexPath) as! UITableViewCell
         
+        let location = APIClient.sharedInstance.studentLocations![indexPath.row]
         studentCell.imageView!.image = UIImage(named: "PinIcon")
-        studentCell.textLabel!.text = "Victor Guerra"
+        studentCell.textLabel!.text = String("\(location.firstName) \(location.lastName)")
         
         return studentCell
     }
     
-    func dummySelector() {
-        println("dummy proc called")
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let studentLocation = APIClient.sharedInstance.studentLocations![indexPath.row]
+        openMediaURL(studentLocation.mediaURL)
     }
-
     
-    // conforming to CommonNavigationBar protocol
+    // MARK: conforming to CommonNavigationBar protocol
     func refresh() {
-        println("refreshing")
+        refreshLocationsWihtHandler() {
+            self.locationsTable.reloadData()
+        }
     }
     
     func showInfoPostingView () {
-        let controller = self.storyboard!.instantiateViewControllerWithIdentifier("InfoPostingViewController") as! InfoPostingViewController
-        self.presentViewController(controller, animated: true, completion: nil)
+        self.locationOverwrite()
     }
 
     func logOut() {
-        APIClient.sharedInstance.logOutFromUdacity() { error in
-            if error == nil {
-                self.dismissViewControllerAnimated(true, completion: nil)
-            }
-        }
+        logOutApp()
     }
-
 }
