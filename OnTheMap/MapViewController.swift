ce41a4f066568b23226ee2f779628c50986a68e3
Victor Guerra <vguerra@gmail.com>
2015-07-27 00:51:02 +0200

    Moved much of the code to the SLViewController and now conforming to MKMapViewDelegate protocol.

--- OnTheMap/MapViewController.swift
+++ OnTheMap/MapViewController.swift
@@ -9,62 +9,79 @@
 import UIKit
 import MapKit
 
-class MapViewController : UIViewController, MKMapViewDelegate, CommonNavigationBar {
+class MapViewController : SLViewController, MKMapViewDelegate, CommonNavigationBar {
 
     @IBOutlet weak var map: MKMapView!
     
+    // MARK: ViewController life cycle
     override func  viewDidLoad() {
         super.viewDidLoad()
-        
         addNavigationBar(self)
         self.navigationItem.title = "On The Map"
-        
+        map.delegate = self
     }
     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        self.refresh()
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        if APIClient.sharedInstance.studentLocations == nil {
+            self.refresh()
+        }
     }
     
     // MARK: conforming to CommonNavigationBar protocol
-    
     func refresh() {
-        APIClient.sharedInstance.getStudentLocations() { locations, error in
-            
-            if let locations = locations {
-                var annotations = [MKAnnotation]()
-                
-                for location in locations {
-                    let lat = CLLocationDegrees(location.latitude)
-                    let long = CLLocationDegrees(location.longitude)
-                    
-                    let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
-                    
-                    var annotation = MKPointAnnotation()
-                    annotation.coordinate = coordinate
-                    annotation.title = "\(location.firstName) \(location.lastName)"
-                    annotation.subtitle = location.mediaURL
-                    
-                    // Finally we place the annotation in an array of annotations.
-                    annotations.append(annotation)
-                }
-                self.map.addAnnotations(annotations)
-            } else {
-                println("Error getting locations \(error)")
-            }
+        refreshLocationsWihtHandler() {
+            self.populateMap()
         }
+    }
+    
+    // Placing all pins on the map
+    func populateMap() {
+        self.map.removeAnnotations(self.map.annotations)
+        var annotations = [MKAnnotation]()
+        for location in APIClient.sharedInstance.studentLocations! {
+            let lat = CLLocationDegrees(location.latitude)
+            let long = CLLocationDegrees(location.longitude)
+            
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            
+            var annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "\(location.firstName) \(location.lastName)"
+            annotation.subtitle = location.mediaURL
+            
+            annotations.append(annotation)
+        }
+        self.map.addAnnotations(annotations)
     }
     
     func showInfoPostingView () {
-        let controller = self.storyboard!.instantiateViewControllerWithIdentifier("InfoPostingViewController") as! InfoPostingViewController
-        self.presentViewController(controller, animated: true, completion: nil)
+        self.locationOverwrite()
     }
     
     func logOut() {
-        APIClient.sharedInstance.logOutFromUdacity() { error in
-            if error == nil {
-                self.dismissViewControllerAnimated(true, completion: nil)
-            }
+        logOutApp()
+    }
+    
+    // MARK: Conforming to MKMapViewDelegate
+    func mapView(mapView: MKMapView!, annotationView: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == annotationView.rightCalloutAccessoryView {
+            openMediaURL(annotationView.annotation.subtitle!)
         }
+    }
+
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinColor = .Red
+            pinView!.rightCalloutAccessoryView = UIButton.buttonWithType(.DetailDisclosure) as! UIButton
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        return pinView
     }
 }
